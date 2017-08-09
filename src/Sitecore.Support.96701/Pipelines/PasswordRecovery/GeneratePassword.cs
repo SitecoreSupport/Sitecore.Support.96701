0eb2f5c9b7cb76a5602f8b2ed832a873a9342de1
DK\DTAT <dtat@sitecore.net>
2017-08-09 09:27:03 +0300

    Patch commit

---
+++ src/Sitecore.Support.96701/Pipelines/PasswordRecovery/GeneratePassword.cs
@@ -0,0 +1,34 @@
+﻿namespace Sitecore.Support.Pipelines.PasswordRecovery
+{
+    using Sitecore.Diagnostics;
+    using Sitecore.Pipelines.PasswordRecovery;
+    using System;
+    using System.Web.Security;
+
+    public class GeneratePassword : PasswordRecoveryProcessor
+    {
+        public override void Process(PasswordRecoveryArgs args)
+        {
+            Assert.IsNotNull(args, "args");
+            MembershipUser user = Membership.GetUser(args.Username);
+          
+            #region Modified code
+
+            if (user == null)
+            {
+                args.AbortPipeline();
+            }          
+            if (user.IsLockedOut)
+            {
+                Log.Error("The account of the specified user was locked out.", this);
+                args.AbortPipeline();
+            }
+            else
+            {
+                args.Password = user.ResetPassword();
+            }
+
+            #endregion
+        }
+    }
+}
